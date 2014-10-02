d68f2f5bb37c965f8d67e51b54746cb263f3c6d4
Ankit Rakha <ankit.rakha@gmail.com>
2014-10-02 04:22:32 -0700

    remove redundant change of directory
    remove redundant change of directory

--- voltdbInstall.sh
+++ voltdbInstall.sh
@@ -5,7 +5,7 @@
 voltdbPackageBaseName=$(basename "$voltdbPackage" .tar.gz | sed -e 's/LINUX-//g')
 
 rm -rf /opt/voltdb
-cd "$workDir" && tar -zxvf "$voltdbPackage" -C /opt
+tar -zxvf "$voltdbPackage" -C /opt
 mv /opt/"$voltdbPackageBaseName" /opt/voltdb
 rpm -Uvh "$workDir"/"$verticaClientRPM"
 cp "$verticaDriverFilePath"/vertica-jdbc* "$voltdbDriverFilePath"/
