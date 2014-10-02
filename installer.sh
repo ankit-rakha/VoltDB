13ba66ce714f76d5a75ebb315ffea8e87e1c3625
Ankit Rakha <ankit.rakha@gmail.com>
2014-10-02 03:44:03 -0700

    fix paths
    fix paths

--- installer.sh
+++ installer.sh
@@ -2,12 +2,12 @@
 
 . config
 
-cd && wget http://voltdb.com/downloads/technologies/server/"$voltdbPackage"
+cd "$workDir" && wget http://voltdb.com/downloads/technologies/server/"$voltdbPackage"
 
 for node in {nsql01,nsql02,nsql03}; do
 
 	echo "Installing VoltDB on node $node"
-	scp "$installerScript" "$voltdbPackage" "$verticaClientRPM" "$voltdbUser"@"$node":~/
-	ssh "$voltdbUser"@"$node" ~/"$installerScript"
+	scp "$installerScript" "$voltdbPackage" "$verticaClientRPM" "$voltdbUser"@"$node":"$workDir"/
+	ssh "$voltdbUser"@"$node" "$workDir"/"$installerScript"
 
 done
--- voltdbInstall.sh
+++ voltdbInstall.sh
@@ -5,7 +5,7 @@
 voltdbPackageBaseName=$(basename "$voltdbPackage" .tar.gz | sed -e 's/LINUX-//g')
 
 rm -rf /opt/voltdb
-tar -zxvf ~/"$voltdbPackage" -C /opt
+tar -zxvf "$workDir"/"$voltdbPackage" -C /opt
 mv /opt/"$voltdbPackageBaseName" /opt/voltdb
 rpm -Uvh "$workDir"/"$verticaClientRPM"
 cp "$verticaDriverFilePath"/vertica-jdbc* "$voltdbDriverFilePath"/
