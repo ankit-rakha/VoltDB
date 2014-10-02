6410846170e9b816a53d8878db51efaa8b96372f
Ankit Rakha <ankit.rakha@gmail.com>
2014-10-02 03:59:28 -0700

    fix path
    fix path

--- voltdbInstall.sh
+++ voltdbInstall.sh
@@ -5,7 +5,7 @@
 voltdbPackageBaseName=$(basename "$voltdbPackage" .tar.gz | sed -e 's/LINUX-//g')
 
 rm -rf /opt/voltdb
-tar -zxvf "$workDir"/"$voltdbPackage" -C /opt
+cd "$workDir" && tar -zxvf "$voltdbPackage" -C /opt
 mv /opt/"$voltdbPackageBaseName" /opt/voltdb
 rpm -Uvh "$workDir"/"$verticaClientRPM"
 cp "$verticaDriverFilePath"/vertica-jdbc* "$voltdbDriverFilePath"/
