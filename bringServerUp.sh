a4d4eaba91c87ebc87b4361e1f75e8d13b518a1f
Ankit Rakha <ankit.rakha@gmail.com>
2014-10-02 00:14:45 -0700

    modularization of config options
    modularization of config options

--- bringServerUp.sh
+++ bringServerUp.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-/opt/voltdb/bin/voltdb compile schema.sql;
-scp catalog.jar deployment.xml root@nsql02:~/VOLTDB/
-scp catalog.jar deployment.xml root@nsql03:~/VOLTDB/
-/opt/voltdb/bin/voltdb create catalog.jar -d deployment.xml -H nsql01 -l /root/VOLTDB/LICENSES/license.xml;
+
+. config
+
+"$voltdbBin"/voltdb compile "$workDir"/"$schemaFile";
+
+for node in {nsql02,nsql03}; do
+
+	echo "#!/bin/bash" > bringServerUp_"$node".sh
+	echo ". config" >> bringServerUp_"$node".sh
+	echo ""$voltdbBin"/voltdb create "$workDir"/"$catalogFile" -d "$workDir"/"$deploymentFile" -H "$electedMaster" -l "$licensePath"/"$licenseFile";" >> bringServerUp_"$node".sh
+	chmod +x bringServerUp_"$node".sh
+
+	scp "$workDir"/config "$workDir"/"$catalogFile" "$workDir"/"$deploymentFile" "$workDir"/bringServerUp_"$node".sh "$voltdbUser"@"$node":"$workDir"/
+
+done
+
+"$voltdbBin"/voltdb create "$workDir"/"$catalogFile" -d "$workDir"/"$deploymentFile" -H "$electedMaster" -l "$licensePath"/"$licenseFile";
