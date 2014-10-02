be26bcfc3326e898d75ac2a6379cb86f0a53c0f0
Ankit Rakha <ankit.rakha@gmail.com>
2014-10-02 02:55:51 -0700

    Make JDBC driver class accessible to the VoltDB process
    Make JDBC driver class accessible to the VoltDB process

--- voltdbInstall.sh
+++ voltdbInstall.sh
@@ -5,3 +5,4 @@
 rm -rf /opt/voltdb
 tar -zxvf ~/"$voltdbPackage" -C /opt
 mv /opt/"$voltdbPackageBaseName" /opt/voltdb
+rpm -Uvh ~/"$verticaClientRPM" && cp "$verticaDriverFilePath"/vertica-jdbc* "$voltdbDriverFilePath"/
