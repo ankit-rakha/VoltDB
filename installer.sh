dc27bfa6e14447daec6999dedec7d8ca16ab0102
Ankit Rakha <ankit.rakha@gmail.com>
2014-10-02 03:55:02 -0700

    broadcast config file to other nodes
    broadcast config file to other nodes

--- installer.sh
+++ installer.sh
@@ -2,12 +2,14 @@
 
 . config
 
-cd "$workDir" && wget http://voltdb.com/downloads/technologies/server/"$voltdbPackage"
+cd "$workDir"
+rm -rf "$voltdbPackage"
+wget http://voltdb.com/downloads/technologies/server/"$voltdbPackage"
 
 for node in {nsql01,nsql02,nsql03}; do
 
 	echo "Installing VoltDB on node $node"
-	scp "$installerScript" "$voltdbPackage" "$verticaClientRPM" "$voltdbUser"@"$node":"$workDir"/
+	scp config "$installerScript" "$voltdbPackage" "$verticaClientRPM" "$voltdbUser"@"$node":"$workDir"/
 	ssh "$voltdbUser"@"$node" "$workDir"/"$installerScript"
 
 done
