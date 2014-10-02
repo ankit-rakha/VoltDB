e9584f9e1f419eec702e3e0b76b090bb3aa7d2d4
Ankit Rakha <ankit.rakha@gmail.com>
2014-10-02 00:28:01 -0700

    relevant column name to the data
    relevant column name to the data

--- schema.sql
+++ schema.sql
@@ -1,18 +1,18 @@
 CREATE TABLE googlers
 (
         googlerID integer UNIQUE NOT NULL,
-        googlerName varchar(50) NOT NULL,
+        googlerSearchKeyWord varchar(50) NOT NULL,
         PRIMARY KEY(googlerID)
 );
 
 CREATE TABLE googlersExport
 (
         googlerID integer NOT NULL,
-        googlerName varchar(50) NOT NULL,
+        googlerSearchKeyWord varchar(50) NOT NULL,
 );
 
 EXPORT TABLE googlersExport;
 
 CREATE PROCEDURE teeLoader AS
-        INSERT INTO googlersExport (googlerID, googlerName)
+        INSERT INTO googlersExport (googlerID, googlerSearchKeyWord)
                 VALUES (?,?);
